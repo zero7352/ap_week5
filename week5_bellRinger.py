8de0bd0023a11fabf8ab5cbeacb3f69250562eda
zero7352 <jsierra17@cps.edu>
2025-11-06 18:40:22 +0000

    review slicing

--- week5_bellRinger.py
+++ week5_bellRinger.py
@@ -11,11 +11,18 @@ first_c_index = print(magic.index('c'))
 last_a_index = print(magic.rindex('a'))
 # Advanced Slicing:
 # Given the string alphabet = 'abcdefghijklmnopqrstuvwxyz',
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
 # a. Extract the letters 'hij'.
 # b. Extract every second letter starting from 'a' to 'm'.
+hij = print(alphabet.index('hij'))
+hij2 = print(alphabet[7:10])
 # c. Reverse the entire string using slicing.
-
+every_second = print(alphabet[0:13:2])
 # Problem Set 2: Extracting Information
+reversed_alphabet = print(alphabet [ : : -1])
+i_have_a_dream = "And when this happens, and when we allow freedom ring, when we let it ring from every village and every hamlet, from every state and every city, we will be able to speed up that day when all of God's children, black men and white men, Jews and Gentiles, Protestants and Catholics, will be able to join hands and sing in the words of the old Negro spiritual"
+
+reversed_i_have_a_dream = print(i_have_a_dream [: : -1])
 # From Descriptions:
 # Extract the name of the famous personality from the quote "Ask not what your country can do for you — ask what you can do for your country. - John F. Kennedy"
 
